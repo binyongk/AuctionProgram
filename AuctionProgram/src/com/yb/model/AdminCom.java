65e9f98c6c4b3fd825b3821e5df00c0cf11b6a6e
Playdata <Playdata@DESKTOP-TFT49U4>
2018-07-02 19:39:22 +0900

    cc

--- AuctionProgram/src/com/yb/model/AdminCom.java
+++ AuctionProgram/src/com/yb/model/AdminCom.java
@@ -8,7 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Vector;
 
-public class AdminCom {
+public class AdminCom implements Runnable {
 
 	ServerSocket ss;
 	Vector<Service> v;
@@ -19,19 +19,21 @@ public class AdminCom {
 		
 		v = new Vector<>();
 		
-		//ss = new ServerSocket(3000);
-		
-		//new Thread(this).start();
-		
-		
-		
+		try {
+			ss = new ServerSocket(3000);
+			new Thread(this).start();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
 	}
 	
 	public void run() {  //여러 클라이언트 접속을 위해 wait
 		try {
 			while(true) {
 				Socket s = ss.accept(); 
-				//v.add(new Service(s));
+				v.add(new Service(s));
 			}
 		} catch (IOException e) {
 		    e.printStackTrace();
